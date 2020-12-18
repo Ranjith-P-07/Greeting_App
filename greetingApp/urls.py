ee0f71df497a8a1b1c56f47f469a302eb3ab7d7b
Ranjith P <Ranjithroy704@gmail.com>
2020-12-18 11:01:58 +0530

    "Add: added update and delete routes"

--- greetingApp/urls.py
+++ greetingApp/urls.py
@@ -4,5 +4,8 @@ from .import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('show',views.show, name='show'),
+    path('show/',views.show, name='show'),
+    path('update/<int:id>', views.update, name='update'),
+    path('delete/<int:id>', views.delete, name='delete'),
+
 ]

e28ea33a19db1f595e94d926885a92e568af4ff1
Ranjith P <Ranjithroy704@gmail.com>
2020-12-18 10:58:06 +0530

    "Add: added update function, Delete function"

--- greetingApp/views.py
+++ greetingApp/views.py
@@ -1,18 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import User
 
 
 def index(request):
-
     if request.method == 'POST':
         name = request.POST.get('name')
         message = request.POST.get('message')
-    return render(request,'greetingApp/index.html')
+        user = User(name=name, message=message)
+        user.save()
+    return render(request, 'greetingApp/index.html')
 
 
 def show(request):
     context = {
-        "users" : User.objects.all()
+        "users": User.objects.all()
     }
     return render(request, 'greetingApp/show.html', context)
 
+
+def update(request, id):
+    userid = id
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        msg = request.POST.get('msg')
+        user_object = User(name=name, msg=msg, id=userid)
+        user_object.save()
+        return redirect('/show')
+    else:
+        context = {
+            "users": User.objects.filter(id=userid)
+        }
+        return render(request, 'greetingApp/update.html', context)
+
+
+def delete(request, id):
+    userid = id
+    user = User.objects.get(id=userid)
+    user.delete()
+    return redirect('/show')
